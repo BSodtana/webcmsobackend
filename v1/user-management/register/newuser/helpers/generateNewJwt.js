50fd6e3f0357d7be17ed364acd8d581eae9ba84c
Phuwamin Wongmor <phuwamin_w@cmu.ac.th>
2023-11-04 15:58:21 +0700

    update private key

--- v1/user-management/register/newuser/helpers/generateNewJwt.js
+++ v1/user-management/register/newuser/helpers/generateNewJwt.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+const JS_PRIVATE_KEY = require('../../../../../config/private')
+const PRIVATE_KEY = JS_PRIVATE_KEY
 async function GenerateNewJwt (studentID, role, expIn) {
   const token = jwt.sign({ studentID, role }, PRIVATE_KEY, { expiresIn: expIn })
   return token
