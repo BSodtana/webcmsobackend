e0087169ef64e49894f504d88ae4f2551e2c2ef2
plykung <58039748+plykung@users.noreply.github.com>
2024-08-02 23:21:09 +0700

    feat: add new/edit/delete position to staff recruitment - /project/:projectID/recruitment/:recruitmentID/staff/:positionID/data     - PUT     - DELETE - /project/:projectID/recruitment/:recruitmentID/staff/:positionID/new     - POST

--- v2.1/project/(each-project)/recruitment/(recruitmentID)/position/(positionID)/eachPOSControllers.js
+++ v2.1/project/(each-project)/recruitment/(recruitmentID)/position/(positionID)/eachPOSControllers.js
@@ -2,85 +2,6 @@ const { errorCodeToResponse } = require("../../../../../../_helpers/errorCodeToR
 const { successCodeToResponse } = require("../../../../../../_helpers/successCodeToResponse")
 const eachPOSServices = require("./eachPOSServices")
 
-const getDataSpecificRecruitIDCon = async (req, res) => {
-    try {
-
-        const { projectID, recruitmentID } = req.params
-        const { studentID = 'NO-STD-ID' } = await req?.userData
-
-        const results = await eachPOSServices.getDataSpecificRecruitID(recruitmentID)
-
-        if (results?.projectID !== projectID) {
-            res.status(400).json(errorCodeToResponse("ACCESS-DENIED-OTHER", 'ProjectID not match recruitment ID', studentID))
-        } else {
-            res.status(200).json(successCodeToResponse(results, 'GET-RECRUITMENT-DATA-SUCCESS', recruitmentID, studentID))
-        }
-
-
-    } catch (error) {
-        console.log('getDataSpecificRecruitIDCon', error)
-        res.status(500).json(errorCodeToResponse(error?.code || "INTERNAL-ERROR", error?.desc || 'getDataSpecificRecruitIDCon'))
-    }
-}
-
-const editDataSpecificRecruitIDCon = async (req, res) => {
-    try {
-
-        const { projectID, recruitmentID } = req.params
-        const { studentID = 'NO-STD-ID' } = await req?.userData
-        const data = req.body
-
-        const found = ["participantRecruitID", "staffRecruitID", "studentID", "projectID", "createdDateTime", "updatedDateTime"].some(r => Object.keys(data).includes(r))
-
-
-        // restricted some data to be edited
-        if (found) {
-            res.status(400).json(errorCodeToResponse("RECRUIT-UPDATE-PROTECTED-DATA", recruitmentID, studentID))
-        } else {
-
-            // check recruit projectID
-            const results = await eachPOSServices.getDataSpecificRecruitID(recruitmentID)
-
-            if (results?.projectID !== projectID) {
-                res.status(400).json(errorCodeToResponse("ACCESS-DENIED-OTHER", 'ProjectID not match recruitment ID', studentID))
-            } else {
-                const results = await eachPOSServices.editDataSpecificRecruitID(recruitmentID, data)
-                res.status(200).json(successCodeToResponse(results, 'UPDATE-RECRUITMENT-DATA-SUCCESS', recruitmentID, studentID))
-            }
-
-
-        }
-
-    } catch (error) {
-        console.log('editDataSpecificRecruitIDCon', error)
-        res.status(500).json(errorCodeToResponse(error?.code || "INTERNAL-ERROR", error?.desc || 'editDataSpecificRecruitIDCon'))
-    }
-}
-
-const deleteDataSpecificRecruitIDCon = async (req, res) => {
-    try {
-
-        const { projectID, recruitmentID } = req.params
-        const { confirmDeleted } = req.body
-        const { studentID = 'NO-STD-ID' } = await req?.userData
-
-        // check recruit projectID
-        const results = await eachPOSServices.getDataSpecificRecruitID(recruitmentID)
-
-        if (results?.projectID !== projectID) {
-            res.status(400).json(errorCodeToResponse("ACCESS-DENIED-OTHER", 'ProjectID not match recruitment ID', studentID))
-        } else {
-            const results = await eachPOSServices.deleteDataSpecificRecruitID(recruitmentID, confirmDeleted)
-            res.status(200).json(successCodeToResponse(results, 'DELETE-RECRUITMENT-DATA-SUCCESS', recruitmentID, studentID))
-        }
-
-
-    } catch (error) {
-        console.log('deleteDataSpecificRecruitIDCon', error)
-        res.status(500).json(errorCodeToResponse(error?.code || "INTERNAL-ERROR", error?.desc || 'deleteDataSpecificRecruitIDCon'))
-    }
-}
-
 const getDataStaffSpecificPositionIDCon = async (req, res) => {
     try {
 
@@ -100,12 +21,80 @@ const getDataStaffSpecificPositionIDCon = async (req, res) => {
     }
 }
 
+const createNewStaffPositionCon = async (req, res) => {
+    try {
 
+        const { projectID, recruitmentID, positionID } = req.params
+        const { studentID = 'NO-STD-ID' } = await req?.userData
+        const data = req.body
+
+        const found = ["staffPositionID", "recruitID", "createdDateTime", "updatedDateTime"].some(r => Object.keys(data).includes(r))
+
+        // restricted some data to be edited
+        if (found) {
+            res.status(400).json(errorCodeToResponse("POSITION-UPDATE-PROTECTED-DATA", recruitmentID, studentID))
+        } else {
+            const results = await eachPOSServices.createNewStaffPosition(
+                recruitmentID,
+                data?.positionName || 'ตำแหน่งหน้าที่',
+                data?.maxNumber || 1,
+                data?.isAllowed || 1
+            )
+            res.status(200).json(successCodeToResponse(results, 'CREATE-NEW-STF-POSITION-SUCCESS', results.staffPositionID, studentID))
+        }
+
+
+    } catch (error) {
+        console.log('createNewStaffPositionCon', error)
+        res.status(500).json(errorCodeToResponse(error?.code || "INTERNAL-ERROR", error?.desc || 'createNewStaffPositionCon'))
+    }
+}
+
+const editStaffPositionCon = async (req, res) => {
+    try {
+
+        const { positionID } = req.params
+        const { studentID = 'NO-STD-ID' } = await req?.userData
+        const data = req.body
+
+        const found = ["recruitID", "createdDateTime", "updatedDateTime"].some(r => Object.keys(data).includes(r))
+
+
+        // restricted some data to be edited
+        if (found) {
+            res.status(400).json(errorCodeToResponse("POSITION-UPDATE-PROTECTED-DATA", positionID, studentID))
+        } else {
+            const results = await eachPOSServices.editStaffPositionData(positionID, data)
+            res.status(200).json(successCodeToResponse(results, 'UPDATE-STF-POSITION-SUCCESS', positionID, studentID))
+        }
+
+    } catch (error) {
+        console.log('editStaffPositionCon', error)
+        res.status(500).json(errorCodeToResponse(error?.code || "INTERNAL-ERROR", error?.desc || 'editStaffPositionCon'))
+    }
+}
+
+const deleteStaffPositionCon = async (req, res) => {
+    try {
+
+        const { positionID } = req.params
+        const { confirmDeleted } = req.body
+        const { studentID = 'NO-STD-ID' } = await req?.userData
+
+        const results = await eachPOSServices.deleteStaffPositionData(positionID, confirmDeleted)
+        res.status(200).json(successCodeToResponse(results, 'DELETE-STF-POSITION-SUCCESS', positionID, studentID))
+
+
+
+    } catch (error) {
+        console.log('deleteStaffPositionCon', error)
+        res.status(500).json(errorCodeToResponse(error?.code || "INTERNAL-ERROR", error?.desc || 'deleteStaffPositionCon'))
+    }
+}
 
 module.exports = {
-    getDataSpecificRecruitIDCon,
-    editDataSpecificRecruitIDCon,
-    deleteDataSpecificRecruitIDCon,
-
-    getDataStaffSpecificPositionIDCon
+    getDataStaffSpecificPositionIDCon,
+    createNewStaffPositionCon,
+    editStaffPositionCon,
+    deleteStaffPositionCon
 }
