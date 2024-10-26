9c3ef793c942040f7885f68e6380e50cafaef5a8
plykung <58039748+plykung@users.noreply.github.com>
2024-10-26 14:11:47 +0700

    feat: ./activity/:projectID/check-in/bulk

---
+++ v2.1/_helpers/error_text/activityError.js
@@ -0,0 +1,25 @@
+const activityError = {
+    "DEMO-ERROR-NO-STUDENT-ID-SPECIFIED": {
+        "code": "DEMO-ERROR-NO-STUDENT-ID-SPECIFIED",
+        "textTH": "กรุณากรอกรหัสนักศึกษา",
+        "textEN": "Student ID is required",
+        "shouldContactAdmin": false,
+        "shouldTryAgain": false,
+    },
+    "CHECK-IN-ERROR-STUDENT-ID-NOT-JOINED-ACTIVITY": {
+        "code": "CHECK-IN-ERROR-STUDENT-ID-NOT-JOINED-ACTIVITY",
+        "textTH": "ไม่สามารถลงทะเบียนเข้าร่วมกิจกรรมได้เนื่องจากไม่ได้เข้าร่วมกิจกรรม",
+        "textEN": "Cannot check-in because this student was not registered",
+        "shouldContactAdmin": false,
+        "shouldTryAgain": false,
+    },
+    "CHECK-IN-ERROR-STUDENT-ID-CHECK-IN-ALREADY": {
+        "code": "CHECK-IN-ERROR-STUDENT-ID-CHECK-IN-ALREADY",
+        "textTH": "ไม่สามารถลงทะเบียนเข้าร่วมกิจกรรมได้เนื่องจากเข้าร่วมกิจกรรมไปแล้ว",
+        "textEN": "Cannot check-in because this student was already checked-in",
+        "shouldContactAdmin": false,
+        "shouldTryAgain": false,
+    },
+}
+
+module.exports = activityError
---
+++ v2.1/_helpers/success_text/activitySuccess.js
@@ -0,0 +1,18 @@
+const activitySuccess = {
+    "ACTIVITY-GET-CHECK-IN-CODE-SUCCESS": {
+        "code": "ACTIVITY-GET-CHECK-IN-CODE-SUCCESS",
+        "textTH": "รับข้อมูลรหัสเข้าร่วมกิจกรรมสำเร็จ",
+        "textEN": "Get activity check in code success",
+        "shouldContactAdmin": false,
+        "shouldTryAgain": false,
+    },
+    "CHECK-IN-BULK-SUCCESS": {
+        "code": "CHECK-IN-BULK-SUCCESS",
+        "textTH": "ลงทะเบียนคนเข้าร่วมกิจกรรมสำเร็จ",
+        "textEN": "Bulk check-in success",
+        "shouldContactAdmin": false,
+        "shouldTryAgain": false,
+    },
+}
+
+module.exports = activitySuccess
