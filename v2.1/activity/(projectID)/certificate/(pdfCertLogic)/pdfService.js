ee92d7d9c85a83c0238dbe3ed2a494babfc8e4ef
plykung <58039748+plykung@users.noreply.github.com>
2024-12-06 19:22:44 +0700

    fix: styling of PCP Cert

--- v2.1/activity/(projectID)/certificate/(pdfCertLogic)/pdfService.js
+++ v2.1/activity/(projectID)/certificate/(pdfCertLogic)/pdfService.js
@@ -1,4 +1,4 @@
-const PDFDocument = require('pdfkit');
+const PDFDocument = require('pdfkit-table');
 const fs = require('fs');
 const getStream = require('get-stream');
 
@@ -19,7 +19,7 @@ const generatePCPPdf = async () => {
 
         // set all thing
         doc.font(Regular)
-        doc.image(`${__dirname}/asset/background/TP_PCP_TH.png`)
+        doc.image(`${__dirname}/asset/background/TP_CLEAN_PCP_TH.png`)
 
 
         doc.font(Bold).fontSize(185).fillColor('#1c3a4f').text(' ', doc.x + 485, doc.y, defaultMargin);
@@ -32,35 +32,74 @@ const generatePCPPdf = async () => {
 
         doc.font(Bold).fontSize(80).text(' ', defaultMargin);
 
-        doc.font(Bold).fontSize(85).fillColor('#349288').text('ณ ภัทร เสาโกมุท', defaultMargin);
+        doc.font(Bold).fontSize(85).fillColor('#349288').text('กมุทพร อยู่บ้านแพ้ว รัตนโกมล', defaultMargin);
 
         doc.font(Bold).fontSize(10).text(' ', defaultMargin);
 
-        doc.font(Regular).fontSize(37).fillColor('#1c3a4f').text('ได้เข้าร่วมโครงการเตรียมความพร้อมสำหรับการสอบ Timed-Station Examination (TSE) ของกระบวนวิชาระบบโครงกระดูกและกล้ามเนื้อในมนุษย์ สำหรับนักศึกษาแพทย์ชั้นปีที่ 2 มหาวิทยาลัยเชียงใหม่ ประจำปีการศึกษา 1/2567 (Pre-TSE 2567)', {
+        doc.font(Regular).fontSize(37).fillColor('#1c3a4f').text('ได้เข้าร่วมโค (Pre-TSE 2567)', {
             ...defaultMargin,
         });
         doc.font(Regular).fontSize(37).text('เมื่อวันที่ 16 กรกฎาคม พ.ศ. 2567', defaultMargin);
 
-        doc.moveDown();
 
-        doc.font(Regular).fontSize(37).text('ให้ไว้ ณ วันที่ 20 เดือน กันยายน พ.ศ.2567', defaultMargin);
 
-        doc.moveDown();
-        doc.moveDown();
+        doc.fontSize(37).text('ให้ไว้ ณ วันที่ 20 เดือน กันยายน พ.ศ.2567', doc.x, 1010, defaultMargin);
 
 
 
+        const init_pad = 157.5
+        const line_length = 350
+        const spacing = 45
 
+        // const pad2 = init_pad + line_length + spacing
+        // const pad3 = pad2 + line_length + spacing
 
+        // doc.moveTo(doc.x + init_pad, doc.y).lineCap('round').lineWidth(3).fillColor('#a6a6a6').lineTo(doc.x + init_pad + line_length, doc.y).stroke();
+        // doc.moveTo(doc.x + pad2, doc.y).lineCap('round').lineWidth(3).fillColor('#a6a6a6').lineTo(doc.x + pad2 + line_length, doc.y).stroke();
+        // doc.moveTo(doc.x + pad3, doc.y).lineCap('round').lineWidth(3).fillColor('#a6a6a6').lineTo(doc.x + pad3 + line_length, doc.y).stroke();
+
+        // doc.rect(doc.x + 110, doc.y + 10, 1300, 100).stroke();
 
         // const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl. Suspendisse rhoncus nisl posuere tortor tempus et dapibus elit porta. Cras leo neque, elementum a rhoncus ut, vestibulum non nibh. Phasellus pretium justo turpis. Etiam vulputate, odio vitae tincidunt ultricies, eros odio dapibus nisi, ut tincidunt lacus arcu eu elit. Aenean velit erat, vehicula eget lacinia ut, dignissim non tellus. Aliquam nec lacus mi, sed vestibulum nunc. Suspendisse potenti. Curabitur vitae sem turpis. Vestibulum sed neque eget dolor dapibus porttitor at sit amet sem. Fusce a turpis lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;';
 
-        // doc.text(lorem).widthOfString(100);
+        // doc.font(Bold).fontSize(25).text(lorem, doc.x + 110, doc.y + 25, {
+        //     columns: 3,
+        //     columnGap: 10,
+        //     height: 150,
+        //     width: 1300,
+        //     align: 'center'
+        // });
 
+        const commonHeaderStyle = {
+            align: 'center',
+            headerColor: "#ffffff",
+            headerOpacity: 0,
+            width: 400
+        }
 
-        // doc.fontSize(25).text('นศพ. ประธาน จริงใจ');
-        // doc.fontSize(25).text('ประธานโครงการ');
+        const table = {
+            headers: [
+                { label: `นศพ. ชื่อ สกุล`, property: 'ownerName', ...commonHeaderStyle },
+                { label: "ผศ.พญ. จีระนันท์ คุณาชีวะ", property: 'advisorName', ...commonHeaderStyle },
+                { label: "นศพ. พิมพ์ลักษณ์ ชัยจิตติประเสริฐ", property: 'cmsoHeadName', ...commonHeaderStyle },
+            ],
+            rows: [
+                ["ประธานโครงการ", "ผู้ช่วยคณบดีคณะแพทยศาสตร์", "นายกสโมสรนักศึกษาคณะแพทยศาสตร์ ประจำปีการศึกษา 2567"],
+            ],
+        };
 
+        await doc.table(table, {
+            width: 1200,
+            x: doc.x + init_pad,
+            y: 1270,
+            columnSpacing: 1,
+            divider: {
+                header: { disabled: true },
+                horizontal: { disabled: true },
+            },
+            prepareHeader: () => doc.font(Bold).fontSize(25),
+            prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => doc.font(Regular).fontSize(20),
+        });
 
         doc.end();
 
