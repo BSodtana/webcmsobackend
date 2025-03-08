9b3de56e8fb22bf234469d5e91d87a2ab25f2247
plykung <58039748+plykung@users.noreply.github.com>
2025-03-09 00:37:30 +0700

    feat: boilerplate for mail api v2

--- config/mail.js
+++ config/mail.js
@@ -1,4 +1,4 @@
 const MAIL_TOKEN = process.env.MAIL_TOKEN
-const MAIL_ENDPOINT = 'https://w3.med.cmu.ac.th/api/v1/mail'
+const MAIL_ENDPOINT = process.env.MAIL_ENDPOINT_V2 || process.env.MAIL_ENDPOINT || 'https://w3.med.cmu.ac.th/api/v1/mail'
 
 module.exports = { MAIL_TOKEN, MAIL_ENDPOINT }
