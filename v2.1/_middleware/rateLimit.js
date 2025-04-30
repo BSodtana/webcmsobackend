187b7275c7cb477a353562a76fad97e2610b4da9
plykung <10594847-plykung@users.noreply.gitlab.com>
2025-04-30 05:18:17 +0000

    hotfix: Edit rateLimit to 300 req (new)

--- v2.1/_middleware/rateLimit.js
+++ v2.1/_middleware/rateLimit.js
@@ -5,7 +5,7 @@ const rateLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minutes
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
-    limit: 5, // Limit each IP to 4 requests per `window` (here, per 1 minutes)
+    limit: 300, // Limit each IP to 4 requests per `window` (here, per 1 minutes)
     message: async (req, res) => {
         return errorCodeToResponse("RATE-LIMITED", "server")
     }
